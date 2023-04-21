846382b469258cf76ed27bc20c335b3ef31c94d1
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-20 17:52:24 -0700

    added licensing badging, links, and section to markdown function

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const markdown = require('./utils/generateMarkdown.js');
 // Creates an array of questions for user input
 // TODO: NEED TO IMPLEMENT WHEN USER CHOOSES A LICENSE ADD A BADGE FOR THAT LICENSE IN README
 const questions = [
+    {name: 'fullName', message: 'Please input your Full Name: ', type: 'input'},
+    {name: 'yearCreated', message: 'Please input the year you created this project: ', type: 'input'},
     {name: 'title', message: 'What is the title of your project?', type: 'input'},
     {name: 'description', message: 'Please enter a description of your project:', type: 'input'},
     {name: 'installation', message: 'Please enter installation instructions:', type: 'input'},
@@ -18,6 +20,7 @@ const questions = [
     {name: 'test', message: 'Please enter test instructions:', type: 'input'},
     {name: 'questionsGithub', message: 'Please input your github username: ', type: 'input'},
     {name: 'questionsEmail', message: 'Please input your email for users to contact you at: ', type: 'input'}
+    
   ];
 
 // function to use .prompt in order to ask user questions array
